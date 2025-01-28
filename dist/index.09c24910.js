3f3d4c1b2c4567b604de49dafefc9a770b09eded
Hanan Mutlak <hamu2401@student.miun.se>
2025-01-28 11:00:30 +0100

    Initial commit

---
+++ dist/index.09c24910.js
@@ -0,0 +1,6 @@
+// Enkel interaktivitet kan läggas till här
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Webbplatsen har laddats!');
+});
+
+//# sourceMappingURL=index.09c24910.js.map
---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+// Enkel interaktivitet kan läggas till här
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Webbplatsen har laddats!');
+});
