7bc392d7364b09db6cac360b381abd3b9c336049
Hanan Mutlak <hamu2401@student.miun.se>
2025-02-05 15:46:47 +0100

    Updated project structure and optimized build process

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,17 @@
-// Enkel interaktivitet kan läggas till här
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Webbplatsen har laddats!');
-});
+    
+
+    
+
+    const images = document.querySelectorAll('img');
+  
+    images.forEach(img => {
+    
+      img.src = img.src.replace('.jpg', '.webp');  // استبدل الصيغة إلى WebP
+      img.src = img.src.replace('.png', '.avif');  // استبدل الصيغة إلى AVIF
+    });
+  
+    console.log('Optimized images applied!');
+  });
+  
